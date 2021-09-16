e503cb933a820b742298816ded7aa3a56d750246
Alexander Cyon <alex.cyon@gmail.com>
2021-09-16 08:56:02 +0200

    Initial Commit

---
+++ Tritium/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tritium
+//
+//  Created by Alexander Cyon on 2021-09-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tritium/TritiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TritiumApp.swift
+//  Tritium
+//
+//  Created by Alexander Cyon on 2021-09-16.
+//
+
+import SwiftUI
+
+@main
+struct TritiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
